23dac453009f3f9a2fc422b575aa3217b62d0785
JessMotta <jessicalimamotta@gmail.com>
2022-04-05 13:29:29 -0300

    add database json

--- scripts/script.js
+++ scripts/script.js
@@ -1,69 +1,65 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-    showMentors()
-  })
-  
-  // array de mentores
-  let mentores = [
-    {
-      nome: "Larissa Orikava",
-      areaInteresse: "UX Design",
-    },
-    {
-      nome: "Luiz Nola",
-      areaInteresse: "Desenvolvimento Fullstack",
-    },
-    {
-      nome: "Guilherme Silva",
-      areaInteresse: "Marketing",
-    },
-    {
-      nome: "Mariana Mendanha",
-      areaInteresse: "Marketing",
-    },
-  ];
-  
-  // selecionar todos os cards a partir das classes
-  let nomesMentores = document.getElementsByClassName("nomesMentores");
-  let categorias = document.getElementsByClassName("categorias");
-  
-  
-  function createCard(id) {
-    // área onde mostrará os mentores
-    let content = document.querySelector(".conteudo");
-    //card
-    let card = document.createElement("div");
-    card.setAttribute("class", "card");
-    content.appendChild(card);
-  
-    // imagem do mentor dentro do card
-    let image = document.createElement("img");
-    image.setAttribute("class", "card-img-top");
-    image.setAttribute("src", "../assets/user.svg");
-    card.appendChild(image);
-  
-    // card body
-    let cardBody = document.createElement("div");
-    cardBody.setAttribute("class", "card-body");
-    card.appendChild(cardBody);
-  
-    // nome do mentor
-    let nomeMentor = document.createElement("h2");
-    nomeMentor.setAttribute("class", "card-title");
-    nomeMentor.textContent = mentores[id].nome;
-    cardBody.appendChild(nomeMentor);
-  
-    // area de interesse
-    let areaDeInteresse = document.createElement("p");
-    areaDeInteresse.setAttribute("class", "card-text");
-    areaDeInteresse.textContent = mentores[id].areaInteresse;
-    cardBody.appendChild(areaDeInteresse);
+// inicializa os cards assim que o HTML é carregado
+document.addEventListener("DOMContentLoaded", () => {
+  initializeCards();
+});
+
+// armazena a database
+let database = "../database.json";
+
+// inicializar os cards
+function initializeCards() {
+  fetch(database)
+    .then((response) => {
+      return response.json();
+    })
+    .then((dataMentors) => showMentors(dataMentors));
+}
+
+// cria os cards e adiciona a info de cada usuário dentro deles
+function createCard(dataMentors, id) {
+  // área onde mostrará os cards dos mentores
+  let content = document.querySelector(".content");
+
+  // cria a estrutura do card e adiciona a área de conteúdo (content)
+  let card = document.createElement("div");
+  card.setAttribute("class", "card");
+  content.appendChild(card);
+
+  // adiciona a imagem do mentor dentro do card
+  let image = document.createElement("img");
+  image.setAttribute("class", "card-img-top");
+  image.setAttribute("src", "../assets/user.svg");
+  card.appendChild(image);
+
+  // adiciona a estrutura do card body (onde ficarão as demais infos)
+  let cardBody = document.createElement("div");
+  cardBody.setAttribute("class", "card-body");
+  card.appendChild(cardBody);
+
+  // adiciona o nome do mentor
+  let mentorName = document.createElement("h2");
+  mentorName.setAttribute("class", "card-title");
+  mentorName.textContent = dataMentors.mentors[id].name;
+  cardBody.appendChild(mentorName);
+
+  // adiciona a category (área de interesse)
+  let category = document.createElement("p");
+  category.setAttribute("class", "card-text");
+  category.textContent = dataMentors.mentors[id].category;
+  cardBody.appendChild(category);
+
+  // adiciona o botão para ver o perfil do mentor em uma segunda tela
+  let buttonProfile = document.createElement("button");
+  buttonProfile.setAttribute("class", "btn");
+  buttonProfile.setAttribute("class", "btn-primary");
+  buttonProfile.innerHTML = "Ver perfil";
+  cardBody.appendChild(buttonProfile);
+}
+
+// função para inicializar a área de mentores na área de destaque
+// nesse primeiro momento pega os 4 primeiros registrados no Banco de dados
+function showMentors(dataMentors) {
+  for (let i = 0; i < 4; i++) {
+    createCard(dataMentors, i);
   }
-  
-  
-  // função para inicializar a área de mentores na área de destaque
-  // nesse primeiro momento pega os 4 primeiros registrados no DB
-  function showMentors() {
-    for(let i = 0; i<4; i++){
-      createCard(i)
-    }
-  }
+}
