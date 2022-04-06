2e08732a761013cd0dad02f49b09bd9b6bfe5809
JessMotta <jessicalimamotta@gmail.com>
2022-04-06 19:01:07 -0300

    add database json

--- scripts/mentorProfile.js
+++ scripts/mentorProfile.js
@@ -1,55 +1,51 @@
+// inicializa os cards assim que o HTML é carregado
+document.addEventListener("DOMContentLoaded", () => {
+  initializeInfo();
+});
+
 // armazena o banco de dados
-// let database = "../database.json";
+let database = "../database.json";
 
-let mentores = [
-  {
-    name: "Larissa Orikava",
-    category: "UX Design",
-    rating: "4",
-  },
-  {
-    name: "Luiz Nola",
-    category: "Desenvolvimento Fullstack",
-    rating: "5",
-  },
-  {
-    name: "Guilherme Silva",
-    category: "Marketing",
-    rating: "3",
-  },
-  {
-    name: "Mariana Mendanha",
-    category: "Marketing",
-    rating: "4",
-  },
-];
+// simular a entrada do id do mentor
+const idMentor = 0;
 
-// pegar as áreas de nome e categoria da tela
-let mentorName = document.getElementById("mentor-name-profile");
-let category = document.getElementById("category-profile");
-// let rating = document.getElementById
+// inicializar as informações do mentor
+function initializeInfo() {
+  fetch(database)
+    .then((response) => {
+      return response.json();
+    })
+    .then((dataMentors) => showInfo(dataMentors))
+    .catch((error) => {
+      console.log(error);
+    });
+}
 
-// vai receber o id do mentor
-mentorName.textContent = mentores[0].name;
-category.textContent = mentores[0].category;
-let rating = parseFloat(mentores[0].rating);
+function showInfo(dataMentors) {
+  // pegar as áreas de nome e categoria da tela
+  let mentorName = document.getElementById("mentor-name-profile");
+  let category = document.getElementById("category-profile");
 
-// todas as estrelas do HTML
-const stars = document.querySelectorAll(".star-icon");
-console.log(stars);
-let listaStar = document.getElementById("classStar")
+  // vai receber o id do mentor
+  mentorName.textContent = dataMentors.mentors[idMentor].name;
+  category.textContent = dataMentors.mentors[idMentor].category;
+  let rating = parseFloat(dataMentors.mentors[idMentor].rating);
+
+  // todas as estrelas do HTML
+  const stars = document.querySelectorAll(".star-icon");
+  console.log(stars);
+  let listaStar = document.getElementById("classStar");
 
   // para pegar a posição correta no array stars
   let i = rating - 1;
-  // pega o valor do data-avaliacao nesse caso data-avaliacao = 4
+  // pega o valor do data-avaliacao 
   let av = stars[i].attributes[1].value;
   console.log(av);
- 
-// adiciona a classe ativo se o rating(avaliação) for igual a posição do data-avaliacao
-  if(rating == av){
-  stars[i].classList.add("ativo")
-  console.log("funcionou")
-  console.log(stars)
+
+  // adiciona a classe ativo se o rating(avaliação) for igual a posição do data-avaliacao
+  if (rating == av) {
+    stars[i].classList.add("ativo");
+    console.log("funcionou");
+    console.log(stars);
+  }
 }
-
-
