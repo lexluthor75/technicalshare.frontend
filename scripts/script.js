b89e27b242b2ba2f8f91dc8784fbc89fe719fb73
JessMotta <jessicalimamotta@gmail.com>
2022-04-07 17:41:34 -0300

    add cards

--- scripts/script.js
+++ scripts/script.js
@@ -23,41 +23,41 @@ function initializeCards() {
 // cria os cards e adiciona a info de cada usuário dentro deles
 function createCard(dataMentors, id) {
   // área onde mostrará os cards dos mentores
-  let content = document.querySelector(".content");
+  let cards = document.querySelector(".cards");
   
-  // cria a estrutura do card e adiciona a área de conteúdo (content)
+  // cria a estrutura do card e adiciona a área de conteúdo (cards)
   let card = document.createElement("div");
   card.setAttribute("class", "card");
-  content.appendChild(card);
+  cards.appendChild(card);
 
   // adiciona a imagem do mentor dentro do card
   let image = document.createElement("img");
-  image.setAttribute("class", "card-img-top");
-  image.setAttribute("src", "../assets/user.svg");
+  image.setAttribute("class", "card__image");
+  image.setAttribute("src", "../assets/images/user.png");
   card.appendChild(image);
 
   // adiciona a estrutura do card body (onde ficarão as demais infos)
   let cardBody = document.createElement("div");
-  cardBody.setAttribute("class", "card-body d-flex flex-column justify-content-around");
+  cardBody.setAttribute("class", "card__info");
   card.appendChild(cardBody);
 
   // adiciona o nome do mentor
   let mentorName = document.createElement("h2");
-  mentorName.setAttribute("class", "card-title");
+  mentorName.setAttribute("class", "card__title");
   mentorName.textContent = dataMentors.mentors[id].name;
   cardBody.appendChild(mentorName);
 
   // adiciona a category (área de interesse)
   let category = document.createElement("p");
-  category.setAttribute("class", "card-text text-muted");
+  category.setAttribute("class", "card__category");
   category.textContent = dataMentors.mentors[id].category;
   cardBody.appendChild(category);
 
   // adiciona o botão para ver o perfil do mentor em uma segunda tela, ao clicar no botão é identificado o id do mentor
   let buttonProfile = document.createElement("button");
-  buttonProfile.setAttribute("class", "btn btn-warning btn-outline-dark");
+  buttonProfile.setAttribute("class", "card__button");
   // buttonProfile.setAttribute("class", "btn btn-warning btn-outline-dark");
-  buttonProfile.innerHTML = "Ver perfil";
+  buttonProfile.innerHTML = "Saiba +";
   buttonProfile.addEventListener('click', function(e){
     identity = (e.target.id)
     openProfile(identity)
