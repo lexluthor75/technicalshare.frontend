64507eeacf991b061004176a825849f613f6be9b
JessMotta <jessicalimamotta@gmail.com>
2022-04-10 20:07:11 -0300

    start register function to get info

---
+++ scripts/register.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+  initializeForm();
+});
+
+let mentorName = document.getElementById("name");
+let email = document.getElementById("email");
+
+let specialty = document.getElementById("specialty");
+let skill = document.getElementById("skill");
+
+let mentor = document.getElementById("mentor");
+let mentored = document.getElementById("mentored");
+
+let register = document.getElementById("register");
+
+function initializeForm() {
+  register.addEventListener("click", function (e) {
+    console.log(mentorName.value);
+    console.log(email.value);
+    console.log(specialty.value);
+    console.log(skill.value);
+    if (mentor.checked) {
+      console.log(mentor.value);
+    }
+
+    if (mentored.checked) {
+      console.log(mentored.value);
+    }
+  });
+}
